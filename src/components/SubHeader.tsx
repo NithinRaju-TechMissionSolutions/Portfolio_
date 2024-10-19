9572eeeba6a458702860c2b510cfda9b36fe1853
Nithin_Raju <nithinraju242@gmail.com>
2024-10-19 20:29:41 +0530

    three.js bugs fixed

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"; // Add `.js` at the end for compatibility
 import { socialMediaList } from "../utils/commonImports";
 
 export default function SubHeader() {
